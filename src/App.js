3eec7489b8a89877da357510b1b8c9684eafa3e6
tariqfaqira <tariqfaqira@gmail.com>
2023-03-06 15:01:57 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserData } from './redux/actions/test';
 
 function App() {
+  const [isLanguageEnglish, setIsLanguageEnglish] = useState(true)
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getUserData())
   }, [])
   const test = useSelector((state) => state?.test?.test);
   console.log("test", test)
+  const handleChange = () => {
+    setIsLanguageEnglish(!isLanguageEnglish)
+    if(isLanguageEnglish) {
+      const body = document.querySelector("body");
+      body.setAttribute('direction', 'rtl')
+    } else {
+      const body = document.querySelector("body");
+      body.setAttribute('direction', 'ltr')
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => handleChange()}>{isLanguageEnglish ? "AR" : "EN"}</button>
+        <img src={logo} className="App-logo test" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
